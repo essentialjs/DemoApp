06cd77fc3163023aab1a5e108c9fa0eeddda34e7
Henrik Vendelbo <henrik.vendelbo@kaba.com>
2014-10-28 15:18:17 +0100

    router basics

--- client/code/rigged/core.js
+++ client/code/rigged/core.js
@@ -1,6 +1,9 @@
 //= ../../components/modernizr/modernizr.js
 //= ../../components/es5-shim/es5-shim.js
 //= ../../components/essentialjs/essential.js
+!function() {
+//= ../../../lib/client/router.js
+}();
 
 !function(global,document) {
 
